ce8faac83a19aaee4095cc7ff1586f2c7e624d86
배홍식 <hongsik980305@gmail.com>
2021-11-17 17:32:16 +0900

    commit first round

---
+++ app/src/main/java/com/example/cubelab/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.onbid
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import com.example.cubelab.R
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Handler().postDelayed({
+            startActivity(Intent(this,hometab::class.java))
+
+        }, 3000)
+    }
+
+
+}
+
+
+
